e300eb804b43ccd8c6b568be87d4d8ad2cd0192a
Neel Patel <nealp1910@gmail.com>
2022-06-13 21:26:22 -0500

    final push

--- static/js/app.js
+++ static/js/app.js
@@ -1,94 +1,119 @@
-function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata= data.metadata;
-      var resultsarray= metadata.filter(sampleobject => 
-        sampleobject.id == sample);
-      var result= resultsarray[0]
-      var panel = d3.select("#sample-metadata");
-      panel.html("");
-      Object.entries(result).forEach(([key, value]) => {
-        panel.append("h6").text(`${key}: ${value}`);
-      });
-    });
-  }
-    
-  function createBar(sample) {
-  // Using `d3.json`
-  d3.json("samples.json").then((data) => {
-    var samples= data.samples;
-    var resultsarray= samples.filter(sampleobject => 
-        sampleobject.id == sample);
-    var result= resultsarray[0]
-  
-    var ids = result.otu_ids;
-    var labels = result.otu_labels;
-    var values = result.sample_values;
-    
-    var BubbleChart = {
-      xaxis: { title: "OTU ID" },
-      hovermode: "closest",
-      };
-  
-      var BubbleData = [ 
-      {
-        x: ids,
-        y: values,
-        text: labels,
-        mode: "markers",
-        marker: {
-          color: ids,
-          size: values,
-          }
-      }
-    ];
-    Plotly.newPlot("bubble", BubbleData, BubbleChart);
-  
-    var BarData =[
-      {
-        y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
-        x:values.slice(0,10).reverse(),
-        text:labels.slice(0,10).reverse(),
-        type:"bar",
-        orientation:"h"
-  
-      }
-    ];
-  
-    var BarLayout = {
-      title: "Top 10 OTU's Found in Individual",
-    };
-  
-    Plotly.newPlot("bar", BarData, BarLayout);
-  });
-  }
-   
-  function init() {
-  // Selecting specific element from drop down menu
-  var selector = d3.select("#selDataset");
-  
-  // create select list
-  d3.json("samples.json").then((data) => {
-    console.log("The Init() function ran");
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-  
-    const firstSample = sampleNames[0];
-    createBar(firstSample);
-    buildMetadata(firstSample);
-  });
-  }
-  
-  function optionChanged(newID) {
-    // function that runs whenever the dropdown is changed
-  createBar(newID);
-  buildMetadata(newID);
-  }
+// Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// function called, runs init instructions
-// runs only on load and refresh of browser page
-init();
+d3.json(url).then(function(data) {
+    console.log(data);
+});
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// function that contains instructions at page load/refresh
+// function does not run until called
+// code that runs once (only on page load or refresh)
+// create dropdown/select
+d3.json("samples.json").then(function(incomingData) {
+    
+    //Populate the dropdown
+    d3.select("#selDataset")
+        .selectAll("option")
+        .data(incomingData.names)
+        .enter()
+        .append("option")
+        .text(d=>d)
+        .attr("value",d=>d);
+
+    optionChanged(d3.select("#selDataset").property("value"));
+});
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Creating Horizontal Bar Graph
+function createBar(x,y,text) {
+    var data = [{
+        type: 'bar',
+        x: x,
+        y: y,
+        text: text,
+        orientation: 'h'
+    }];
+
+    var layout = {
+        title: "Top 10 OTUs"
+      };
+
+    Plotly.newPlot('bar', data, layout);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Creating Scatter Plot (Bubble Chart)
+function createScatter(x,y,text) {
+    var data = [{
+        x: x,
+        y: y,
+        text: text,
+        mode: 'markers',
+        marker: {
+          size: y,
+          color: x.map(value=>value)
+        }
+    }];
+    var layout = {
+        title: "OTU Values",
+        xaxis: {
+            title: {
+              text: 'OTU ID',
+            }
+        }
+    };
+    Plotly.newPlot('bubble', data, layout);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Creating Gauge Plot
+function createGauge(num) {
+    
+    var data = [
+    {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: num,
+        title: "Belly Button Washing Frequenc",
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+            axis: { range: [null, 10]},
+            bar: { color: "#000000" },
+            steps: [
+                { range: [0, 1], color: "#f1f8e9" },
+                { range: [1, 2], color: "#dcedc8" },
+                { range: [2, 3], color: "#c5e1a5" },
+                { range: [3, 4], color: "#aed581" },
+                { range: [4, 5], color: "#9ccc65" },
+                { range: [5, 6], color: "#8bc34a" },
+                { range: [6, 7], color: "#7cb342" },
+                { range: [7, 8], color: "#689f38" },
+                { range: [8, 9], color: "#558b2f" },
+                { range: [9, 10], color: "#33691e" },
+            ],
+        }
+    }];
+    Plotly.newPlot('gauge', data);
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function Meta(data) {
+    var div = d3.select("#sample-metadata");
+    div.html("")
+    var list = div.append("ul");
+    Object.entries(data).forEach(([key, value]) => {
+        list.append("li").text(key + ": " + value);
+     });
+}
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Code that makes list, paragraph, text/linebreaks at id='sample-meta'
+function optionChanged(value) {
+    d3.json("samples.json").then(function(incomingData) {
+        var metadata = incomingData.metadata.filter(data => data.id ==value);
+        console.log(metadata);
+
+        var sample = incomingData.samples.filter(data => data.id ==value);
+        console.log(sample);
+
+        createBar(sample[0].sample_values.slice(0,10).reverse(),sample[0].otu_ids.slice(0,10).reverse().map(a=>"OTU "+ a),sample[0].otu_labels.slice(0,10).reverse());
+        createScatter(sample[0].otu_ids,sample[0].sample_values,sample[0].otu_labels);
+        Meta(metadata[0]);
+        createGauge(metadata[0].wfreq);
+    });
+}
